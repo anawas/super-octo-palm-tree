4c5fadc750e5e7120f1f6e47e332eb5689540996
Andreas Wassmer <andreas.wassmer@me.com>
2023-12-08 18:07:20 +0100

    Striche statt Sternchen wie gewünscht.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,12 @@ fun main() {
     print("Wie soll ich dich nennen? ")
     val name = readln()
     for (i in 1..10) {
-        print("*")
+        print("-")
     }
     println()
     println("Hallo $name")
     println("Heute ist ein wundervoller Tag")
     for (i in 1..10) {
-        print("*")
+        print("-")
     }
 }
