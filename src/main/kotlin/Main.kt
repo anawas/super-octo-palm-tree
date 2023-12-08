c9e83d47f192c0e676d60b9825f86cb689d7883a
Andreas Wassmer <andreas.wassmer@me.com>
2023-12-08 19:46:59 +0100

    Sie-Form

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ fun main() {
     val messages = listOf("Heute ist ein wundervoller Tag", "Nutze den Tag", "Heute ist dein Glückstag")
     val numOfSymbols = 20
 
-    print("Wie soll ich dich nennen? ")
+    print("Wie soll ich Sie nennen? ")
     val name = readln()
     for (i in 1..numOfSymbols) {
         print("-")
