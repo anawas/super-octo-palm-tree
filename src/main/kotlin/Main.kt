4e03af75149fb302ce448f37a3256b7e6496afb9
Andreas Wassmer <andreas.wassmer@me.com>
2023-12-08 18:09:00 +0100

    Mehr Symbole als Trenner

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,14 @@
 fun main() {
+    val numOfSymbols = 20
     print("Wie soll ich dich nennen? ")
     val name = readln()
-    for (i in 1..10) {
+    for (i in 1..numOfSymbols) {
         print("-")
     }
     println()
     println("Hallo $name")
     println("Heute ist ein wundervoller Tag")
-    for (i in 1..10) {
+    for (i in 1..numOfSymbols) {
         print("-")
     }
 }
